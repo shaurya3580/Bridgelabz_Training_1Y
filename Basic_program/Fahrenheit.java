38886e82cbaeecd9f1faf11a4920ea324fb8ee49
shaurya3580 <nameshaurya@gmail.com>
2026-01-21 14:37:11 +0530

    main clean

--- Basic_program/Fahrenheit.java
+++
@@ -1,9 +0,0 @@
-class Fahrenheit
-{
-	public static void main(String args[]) {
-		int celcius = 10;
-		double Fahrenheit = (celcius*9/5) + 32;
-		
-		System.out.println("temp:-" + Fahrenheit);
-	}
-}
